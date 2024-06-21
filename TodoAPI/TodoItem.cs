8efcb6733a3d625b03dd2cc5f6766e2be7945e9a
gm50x <getuliomagela@outlook.com>
2024-06-21 15:46:22 -0300

    Add project files.

---
+++ TodoAPI/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoAPI
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
